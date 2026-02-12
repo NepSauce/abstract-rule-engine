3eea67542515dd954d656c0d082d5e67385ac949
NepSauce <greasyandmoist@gmail.com>
2026-02-12 19:25:29 -0400

    Feat: Implement grid rendering and JSON rule reading functionality

---
+++ util/json_reader.py
@@ -0,0 +1,10 @@
+import json
+
+class JsonReader:
+    @staticmethod
+    
+    def read_json_file(file_path):
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+
